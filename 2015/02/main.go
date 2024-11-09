95fc2e187498509620ab106a78fd86ce3c6a946e
izzany <izzanyzaini@gmail.com>
2024-11-09 12:01:19 +0800

    wip

--- 2015/02/main.go
+++ 2015/02/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
-) 
+)
+
+type Dimension struct {
+	length int
+	width  int
+	height int
+}
 
 func main() {
-    fi, _ := os.ReadFile("./input.txt")
-    fiarr := strings.Split(string(fi), "\\s+")
-    fmt.Print(fiarr)    
+	fi, _ := os.Open("./input.txt")
+	fiarr := bufio.NewScanner(fi)
+	fmt.Print(fiarr)
+
+	var volumetotal int
+
+	for fiarr.Scan() {
+		line := fiarr.Text()
+
+		x := func(c rune) bool {
+			return c == 'x'
+		}
+
+		fields := strings.FieldsFunc(line, x)
+		dimension := Dimension{strconv.Atoi(fields[0]), strconv.Atoi(fields[1]), strconv.Atoi(fields[2])}
+		d := &dimension
+
+		volumetotal += getVolume(d)
+
+	}
 
 }
 
-func getVolume(length, width, height int) int {
-    
-    return 0;
+func getDimension() struct{}
+
+func getVolume(class Dimension) int {
+
+	return 0
 }
